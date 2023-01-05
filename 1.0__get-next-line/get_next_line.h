de29015d2cd455cf2bb7c054f1e8db84eed913b1
juansimmendinger <juanrossi.1995@gmail.com>
2023-01-05 12:18:10 +0100

    get_next_line

---
+++ 1.0__get-next-line/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsimmend <jsimmend@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/05 12:17:37 by jsimmend          #+#    #+#             */
+/*   Updated: 2023/01/05 12:17:40 by jsimmend         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <sys/uio.h>
+# include <sys/types.h>
+
+char	*get_next_line(int fd);
+
+#endif
